65f3ecf9a1c087343ac9789009e482fe3eaea4e9
Dong-Jie-Chen <cdjchen@ucdavis.edu>
2022-01-25 13:50:18 -0800

    print performance when load the result

--- script/stage_2_script/script_load_result.py
+++ script/stage_2_script/script_load_result.py
@@ -1,9 +1,17 @@
 from codes.stage_2_code.Result_Loader import Result_Loader
+from codes.stage_2_code.Evaluate_Accuracy import Evaluate_Accuracy
 
 if 1:
     result_obj = Result_Loader('saver', '')
-    result_obj.result_destination_folder_path = '../../result/stage_2_result/SVM_'
+    result_obj.result_destination_folder_path = '../../result/stage_2_result/MLP_'
     result_obj.result_destination_file_name = 'prediction_result'
 
     result_obj.load()
     print('Result:', result_obj.data)
+
+    evaluate_obj = Evaluate_Accuracy('accuracy', '')
+    evaluate_obj.data = result_obj.data
+    mean_score = evaluate_obj.evaluate()
+    print('************ Overall Performance ************')
+    print('MLP Accuracy: ' + str(mean_score))
+    print('************ Finish ************')
